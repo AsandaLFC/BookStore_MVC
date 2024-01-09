549a4f36f628e5519b864b9dbc889b5a871f8f13
AsandaLFC <106383335+AsandaLFC@users.noreply.github.com>
2024-01-09 11:44:55 +0200

    update

--- BookStore/Data/ApplicationDbContext.cs
+++ BookStore/Data/ApplicationDbContext.cs
@@ -1,7 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BookStore.Models;
+using Microsoft.EntityFrameworkCore;
 namespace BookStore.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :base(options)
+        {
+                   
+        }
+         public DbSet<Category> Categories { get; set; }
     }
 }
