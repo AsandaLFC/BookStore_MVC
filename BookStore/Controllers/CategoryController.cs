c5846cb1e2dd9b2f00e11c7d6dc642b4536e3792
AsandaLFC <106383335+AsandaLFC@users.noreply.github.com>
2024-01-21 17:56:59 +0200

    update

--- BookStore/Controllers/CategoryController.cs
+++ BookStore/Controllers/CategoryController.cs
@@ -1,11 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookStore.Data;
+using BookStore.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BookStore.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db; 
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
+            List<Category> objcategoryList = _db.Categories.ToList();
             return View();
         }
     }
--- BookStore/Data/ApplicationDbContext.cs
+++ BookStore/Data/ApplicationDbContext.cs
@@ -9,5 +9,15 @@ namespace BookStore.Data
                    
         }
          public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { ID = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { ID = 2, Name = "SciFi", DisplayOrder = 2 },
+                new Category { ID = 3, Name = "history", DisplayOrder = 3 }
+          );
+
+        }
     }
 }
